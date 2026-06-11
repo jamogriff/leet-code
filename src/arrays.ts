1bb14c43ad76a82e355ac4d703ce966659e307f3
Jamison Griffith <jamison.griffith@gmail.com>
2026-06-11 14:53:17 -0600

    Adds problems and solutions for anagrams

--- src/arrays.ts
+++ src/arrays.ts
@@ -1,5 +1,7 @@
+/*
+ * TODO: should refactor/attempt again for more efficient solution
+ */
 export function two_sum(array, targetSum) {
-  // TODO: implement more efficient solution
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array.length; j++) {
       if (i === j) {
@@ -28,3 +30,67 @@ export function contains_duplicate(array) {
 
   return containsDuplicate;
 }
+
+export function is_anagram(string1, string2) {
+  if (string1.length !== string2.length) {
+    return false;
+  }
+
+  let containsAnagram = true;
+  for (const letter of string1) {
+    if (!string2.includes(letter)) {
+      containsAnagram = false;
+      break;
+    }
+  }
+
+  return containsAnagram;
+}
+
+/*
+ * TODO: should refactor/attempt again
+ */
+export function group_anagrams(possibleAnagrams) {
+  let groupedAnagrams = [];
+  let indiciesMatched = [];
+  for (let i = 0; i < possibleAnagrams.length; i++) {
+    for (let j = 0; j < possibleAnagrams.length; j++) {
+      if (
+        i === j ||
+        indiciesMatched.includes(i) ||
+        indiciesMatched.includes(j)
+      ) {
+        continue;
+      }
+
+      if (is_anagram(possibleAnagrams[i], possibleAnagrams[j])) {
+        // Check output array to find correct place to insert
+        const insertedIntoGroup = false;
+        for (let k = 0; k < groupedAnagrams.length; k++) {
+          if (is_anagram(groupedAnagrams[k][0], possibleAnagrams[i])) {
+            const newGroup = [...groupedAnagrams[k], possibleAnagrams[i], possibleAnagrams[j]];
+            groupedAnagrams[k] = newGroup;
+            insertedIntoGroup = true;
+            break;
+          }
+        }
+        
+        if (!insertedIntoGroup) {
+          groupedAnagrams.push([possibleAnagrams[i], possibleAnagrams[j]]);
+        }
+
+        // Log matched indicies so duplicates are not added
+        let newIndiciesMatched = [...indiciesMatched, i, j];
+        indiciesMatched = newIndiciesMatched;
+      }
+    }
+
+    if (!indiciesMatched.includes(i)) {
+      groupedAnagrams.push([possibleAnagrams[i]]);
+      indiciesMatched.push(i);
+    }
+  }
+
+  return groupedAnagrams;
+}
+
--- test/arrays.spec.ts
+++ test/arrays.spec.ts
@@ -1,5 +1,10 @@
 import { expect, test } from 'vitest';
-import { two_sum, contains_duplicate } from '../src/arrays.ts';
+import { 
+    two_sum,
+    contains_duplicate,
+    is_anagram,
+    group_anagrams
+} from '../src/arrays.ts';
 
 test('returns correct indicies of two sum problems', () => {
     let array = [2,10,5,9,30];
@@ -21,4 +26,16 @@ test('correctly detects duplicate values in array', () => {
 
     array = [3,4,5,6,7,8,9,5];
     expect(contains_duplicate(array)).toBe(true);
+})
+
+test('correctly detects anagrams', () => {
+    expect(is_anagram('ling', 'ring')).toBe(false)
+    expect(is_anagram('adobe', 'beado')).toBe(true)
+    expect(is_anagram('bling', 'bing')).toBe(false)
+})
+
+test('correctly groups anagrams', () => {
+    let array = ['bling', 'ring', 'grin', 'bing', 'glinb']
+    // TODO: this test could be better. Fails when order of expected elements is different
+    expect(group_anagrams(array)).toStrictEqual([['bling', 'glinb'], ['ring', 'grin'], ['bing']])
 })
